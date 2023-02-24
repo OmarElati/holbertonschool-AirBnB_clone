76b2c3a12ab6562928d0b890831a84ef47538682
OmarElati <5796@holbertonstudents.com>
2023-02-24 01:26:19 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,40 @@
+#!/usr/bin/python3
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-    """Defines all common attributes/methods for other classes"""
-    
-    def __init__(self):
-        """Initialize the BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
-    def __str__(self):
-        """Return a string representation of the BaseModel instance"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
-    def save(self):
-        """Update the attribute updated_at with the current datetime"""
-        self.updated_at = datetime.now()
-    
+    """The base model class"""
+    def __init__(self, *args, **kwargs):
+        """Initialize the base model"""
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __repr__(self):
+        """Return a string representation of the object"""
+        return f"<{self.__class__.__name__} {self.id}>"
+
     def to_dict(self):
-        """Return a dictionary containing all keys/values of the instance"""
-        my_dict = dict(self.__dict__)
-        my_dict['__class__'] = self.__class__.__name__
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
-        return my_dict
+        """Return a dictionary representation of the object"""
+        data = {}
+        for key, value in self.__dict__.items():
+            if key.startswith('_'):
+                continue
+            if isinstance(value, datetime):
+                data[key] = value.isoformat()
+            else:
+                data[key] = value
+        return data
+
+    def save(self):
+        """Save the object"""
+        self.updated_at = datetime.now()
+
+    def delete(self):
+        """Delete the object"""
+        pass
--- models/tmp_base_model.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
-
-class MyModel(BaseModel):
-    def save(self):
-        pass
