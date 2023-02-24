3496c49cddc3312bc448b7bc6d8cd7e22f4ef91e
OmarElati <5796@holbertonstudents.com>
2023-02-24 05:09:46 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+import models
 from datetime import datetime
 import uuid
-from models import storage
 
 
 class BaseModel:
@@ -19,14 +19,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
