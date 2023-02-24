34b958f403e9e87a38e6f979b3a0df9c36373fcc
OmarElati <5796@holbertonstudents.com>
2023-02-24 16:51:14 +0100

    test

--- console.py
+++ console.py
@@ -1,13 +1,101 @@
-#!/usr/bin/env python3
-"""Module for HBNB console"""
-
+#!/usr/bin/python3
+"""Command interpreter module"""
 import cmd
+import models
+from models import models_classes
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    """Class for HBNB console"""
-
+    """Command interpreter class"""
     prompt = "(hbnb) "
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            instance = storage.all()[args[0] + "." + args[1]]
+            print(instance)
+        except KeyError:
+            print("** no instance found **")
+        except IndexError:
+            if args[0] not in storage.classes:
+                print("** class doesn't exist **")
+            else:
+                print("** instance id missing **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            instance = storage.all()[args[0] + "." + args[1]]
+            del storage.all()[args[0] + "." + args[1]]
+            storage.save()
+        except KeyError:
+            print("** no instance found **")
+        except IndexError:
+            if args[0] not in storage.classes:
+                print("** class doesn't exist **")
+            else:
+                print("** instance id missing **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        objects = []
+        if arg:
+            if arg not in storage.classes:
+                print("** class doesn't exist **")
+                return
+            for k, v in storage.all().items():
+                if arg in k:
+                    objects.append(str(v))
+        else:
+            for v in storage.all().values():
+                objects.append(str(v))
+        print(objects)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            instance = storage.all()[args[0] + "." + args[1]]
+            if len(args) < 3:
+                print("** attribute name missing **")
+                return
+            if len(args) < 4:
+                print("** value missing **")
+                return
+            setattr(instance, args[2], type(getattr(instance, args[2]))(args[3]))
+            instance.save()
+        except KeyError:
+            print("** no instance found **")
+        except IndexError:
+            if args[0] not in storage.classes():
+                print("** class doesn't exist **")
+            else:
+                print("** instance id missing **")
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -15,12 +103,33 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """EOF command to exit the program"""
-        print()
         return True
 
     def emptyline(self):
-        """Do nothing when an empty line is entered"""
+        """Empty line"""
         pass
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in models_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = class_name + '.' + instance_id
+        if key not in models.storage.all():
+            print("** no instance found **")
+            return
+        del models.storage.all()[key]
+        models.storage.save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,98 +3,116 @@
 Unittest for BaseModel class
 """
 import unittest
-import os
+import models
+from models import storage
 from datetime import datetime
 from models.base_model import BaseModel
+from time import sleep
 
 
-class TestBaseModel(unittest.TestCase):
-    """
-    Test cases for BaseModel class
-    """
+class TestBaseModel_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the BaseModel class."""
 
-    def setUp(self):
-        """
-        Set up test environment
-        """
-        self.model = BaseModel()
+    def test_no_args_instantiates(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
 
-    def tearDown(self):
-        """
-        Tear down test environment
-        """
-        del self.model
-        try:
-            os.remove("file.json")
-        except:
-            pass
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(BaseModel(), models.storage.all().values())
 
-    def test_instance_type(self):
-        """
-        Test instance type
-        """
-        self.assertIsInstance(self.model, BaseModel)
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(BaseModel().id))
 
-    def test_id_generation(self):
-        """
-        Test id generation
-        """
-        model1 = BaseModel()
-        model2 = BaseModel()
-        self.assertNotEqual(model1.id, model2.id)
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
 
-    def test_created_at_type(self):
-        """
-        Test created_at type
-        """
-        self.assertIsInstance(self.model.created_at, datetime)
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
 
-    def test_updated_at_type(self):
-        """
-        Test updated_at type
-        """
-        self.assertIsInstance(self.model.updated_at, datetime)
+    def test_two_models_unique_ids(self):
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm1.id, bm2.id)
 
-    def test_save(self):
-        """
-        Test save method
-        """
-        self.model.save()
-        self.assertNotEqual(self.model.created_at, self.model.updated_at)
+    def test_two_models_different_created_at(self):
+        bm1 = BaseModel()
+        sleep(0.05)
+        bm2 = BaseModel()
+        self.assertLess(bm1.created_at, bm2.created_at)
 
-    def test_to_dict(self):
-        """
-        Test to_dict method
-        """
-        dict_ = self.model.to_dict()
-        self.assertIsInstance(dict_, dict)
-        self.assertEqual(dict_["id"], self.model.id)
-        self.assertEqual(dict_["created_at"], self.model.created_at.isoformat())
-        self.assertEqual(dict_["updated_at"], self.model.updated_at.isoformat())
-        self.assertEqual(dict_["__class__"], "BaseModel")
+    def test_two_models_different_updated_at(self):
+        bm1 = BaseModel()
+        sleep(0.05)
+        bm2 = BaseModel()
+        self.assertLess(bm1.updated_at, bm2.updated_at)
 
-    def test_init_dict(self):
-        """
-        Test __init__ method with dictionary
-        """
-        dict_ = self.model.to_dict()
-        model2 = BaseModel(**dict_)
-        self.assertNotEqual(self.model, model2)
-        self.assertEqual(self.model.id, model2.id)
-        self.assertEqual(self.model.created_at, model2.created_at)
-        self.assertEqual(self.model.updated_at, model2.updated_at)
+    def test_str_representation(self):
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        bm = BaseModel()
+        bm.id = "123456"
+        bm.created_at = bm.updated_at = dt
+        bmstr = bm.__str__()
+        self.assertIn("[BaseModel] (123456)", bmstr)
+        self.assertIn("'id': '123456'", bmstr)
+        self.assertIn("'created_at': " + dt_repr, bmstr)
+        self.assertIn("'updated_at': " + dt_repr, bmstr)
 
-    def test_init_new(self):
-        """
-        Test __init__ method with new instance
-        """
-        model1 = BaseModel()
-        model2 = BaseModel(id=model1.id, created_at=model1.created_at,
-                            updated_at=model1.updated_at)
-        self.assertNotEqual(model1, model2)
-        self.assertEqual(model1.id, model2.id)
-        self.assertEqual(model1.created_at, model2.created_at)
-        self.assertEqual(model1.updated_at, model2.updated_at)
+    def test_args_unused(self):
+        bm = BaseModel(None)
+        self.assertNotIn(None, bm.__dict__.values())
+
+    def test_instantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            BaseModel(id=None, created_at=None, updated_at=None)
+
+class Test_BaseModel_to_dict(unittest.TestCase):
+    """Unittests for testing to_dict method of the BaseModel class."""
+
+    def test_to_dict_type(self):
+        bm = BaseModel()
+        self.assertTrue(dict, type(bm.to_dict()))
+
+    def test_to_dict_contains_correct_keys(self):
+        bm = BaseModel()
+        self.assertIn("id", bm.to_dict())
+        self.assertIn("created_at", bm.to_dict())
+        self.assertIn("updated_at", bm.to_dict())
+        self.assertIn("__class__", bm.to_dict())
+
+    def test_to_dict_contains_added_attributes(self):
+        bm = BaseModel()
+        bm.name = "Holberton"
+        bm.my_number = 98
+        self.assertIn("name", bm.to_dict())
+        self.assertIn("my_number", bm.to_dict())
+
+    def test_to_dict_datetime_attributes_are_strs(self):
+        bm = BaseModel()
+        bm_dict = bm.to_dict()
+        self.assertEqual(str, type(bm_dict["created_at"]))
+        self.assertEqual(str, type(bm_dict["updated_at"]))
+
+    def test_to_dict_output(self):
+        dt = datetime.today()
+        bm = BaseModel()
+        bm.id = "123456"
+        bm.created_at = bm.updated_at = dt
+        tdict = {
+            'id': '123456',
+            '__class__': 'BaseModel',
+            'created_at': dt.isoformat(),
+            'updated_at': dt.isoformat()
+        }
+        self.assertDictEqual(bm.to_dict(), tdict)
+
+    def test_contrast_to_dict_dunder_dict(self):
+        bm = BaseModel()
+        self.assertNotEqual(bm.to_dict(), bm.__dict__)
+
+    def test_to_dict_with_arg(self):
+        bm = BaseModel()
+        with self.assertRaises(TypeError):
+            bm.to_dict(None)
 
 
 if __name__ == "__main__":
