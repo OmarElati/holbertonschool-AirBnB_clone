cb9fee6811887bb6056cae81f5628b052da2897d
OmarElati <5796@holbertonstudents.com>
2023-02-24 22:59:23 +0100

    test

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models import storage
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+    storage = storage
 
 storage = FileStorage()
 storage.reload()
